1701b8af40d7f6f9fcc56d7dba175abf5fb592d6
Rashid Wassan <rashidwassaan@gmail.com>
2022-08-24 16:03:09 +0500

    fix javascript processing

--- lib/screens/webview_page.dart
+++ lib/screens/webview_page.dart
@@ -35,6 +35,7 @@ class AWSRegistrationWebViewState extends State<AWSRegistrationWebView> {
               )),
         ),
         body: const WebView(
+          javascriptMode: JavascriptMode.unrestricted,
           zoomEnabled: false,
           initialUrl: 'https://awscommunity.pk/aws-community-day-lahore-2022/',
         ),
